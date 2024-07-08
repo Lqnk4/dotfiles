f7d783b48f9e82c70a5766e8f8fb2a88e0aba1c1
Lqnk4 <internalbeast109@gmail.com>
2024-07-08 02:25:58 -0700

    swappy

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -19,10 +19,3 @@ map("n", "]b", ":bn<cr>", {})
 -- move lines up or down
 map( {'n', 'i', 'v' }, '<A-j>', '<cmd>:m .+1<CR>')
 map( {'n', 'i', 'v' }, '<A-k>', '<cmd>:m .-2<CR>')
-
-
--- note: diagnostics are not exclusive to lsp servers
--- so these can be global keybindings
-vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
